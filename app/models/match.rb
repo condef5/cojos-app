1a65d8c767ad0660ae70a96cae407348f61a8c97
condef5 <frankcondezo@gmail.com>
2022-02-09 20:04:10 -0500

    add relations to player and match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,5 @@
 class Match < ApplicationRecord
+  has_many :players, dependent: :destroy
+
   validates :place, :time, presence: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
+  belongs_to :match
+
   validates :full_name, presence: true
 end
