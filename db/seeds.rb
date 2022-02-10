c9374d1b548a1b65e7f546665e1abb6c1a2e6d4a
condef5 <frankcondezo@gmail.com>
2022-02-09 21:19:28 -0500

    add initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Player.destroy_all
+Match.destroy_all
+
+match = Match.create(place: 'Potokar', time: DateTime.parse('2022-02-09 21:00:00'))
+Player.create([
+  { full_name: 'Frank Condezo', match_id: match.id },
+  { full_name: 'Angel Cerna', match_id: match.id }
+])
