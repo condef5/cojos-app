259e59eb64752bc5ab2518ac95cd3413e236e1d8
condef5 <frankcondezo@gmail.com>
2022-02-11 20:35:12 -0500

    add pay_at attribute to players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_030650) do
+ActiveRecord::Schema.define(version: 2022_02_12_013411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_030650) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "match_id", null: false
+    t.datetime "pay_at", precision: 6
     t.index ["match_id"], name: "index_players_on_match_id"
   end
 
