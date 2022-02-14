bd91192fd3c6c06860517da2616b7e79adbf31d3
condef5 <frankcondezo@gmail.com>
2022-02-14 18:29:54 -0500

    fix date
    Signed-off-by: condef5 <frankcondezo@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @current_match = Match.where('time > ?', Time.now).first
-    @players = @current_match.players.order(created_at: :desc) if @current_match
+    @current_match = Match.last
+    @players = @current_match.players if @current_match
     @new_player = Player.new
   end
 end
